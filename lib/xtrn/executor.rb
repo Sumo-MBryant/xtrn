68cdb74324dd14def18ef1f7ca998c4026417a65
Viktor Vasilev <viktor_vasilev@scee.net>
2015-10-27 18:33:07 +0000

    Allow SVN v1.6 clients which do not support "upgrade" to fail silently.

--- lib/xtrn/executor.rb
+++ lib/xtrn/executor.rb
@@ -12,12 +12,12 @@ module Xtrn
       @verbose  = opts[:verbose]
     end
 
-    def exec(cmd)
+    def exec(cmd, ignore_failure=false)
       puts "Exec: #{cmd}" if @verbose
 
       stdout, stderr, status = Open3.capture3(cmd)
 
-      unless status.success?
+      unless status.success? or ignore_failure
         puts "Aborting due to execution error:\n#{stderr}"
         exit(1)
       end
